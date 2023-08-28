9e02ecf6814d136a6d8f73c5fff41110d8892ede
Snehjaiswal <snehj121@gmail.com>
2023-08-28 11:01:43 +0530

    ddd

--- FRONTEND/src/Components/Dashboard/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div><div class="footer"><div class="copyright"><p>Copyright © Designed &amp; Developed by <a href="#" target="_blank">PNP Infotech 2023</a> 2022</p></div></div></div>
-  )
-}
-
-export default Footer
