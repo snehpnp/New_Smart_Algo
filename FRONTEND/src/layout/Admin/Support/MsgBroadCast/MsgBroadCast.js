f891e0c970ff35e513ab9cab1b2e3bfd1535367c
Neha <93382562+Neha4581@users.noreply.github.com>
2023-08-29 18:09:45 +0530

    code  neha

--- FRONTEND/src/layout/Admin/Support/MsgBroadCast/MsgBroadCast.js
+++ FRONTEND/src/layout/Admin/Support/MsgBroadCast/MsgBroadCast.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Content from "../../../../Components/Dashboard/Content/Content"
-
+import Formikform from "../../../../Components/ExtraComponents/Form/Formik_form"
+import { useFormik } from 'formik';
 
 const MsgBroadCast = () => {
 
+    const fields = [
+        { name: 'strategy', label: 'Strategy', type: 'text' },
+    ]
+    const formik = useFormik({
+        initialValues: {
+            strategy: '',
+        },
+    })
     return <>
         <Content Page_title="Message Broadcast">
-            <p>MsgBroadCast</p>
+        <Formikform 
+        fieldtype={fields.filter(field => !field.showWhen)} 
+        formik={formik} 
+        btn_name="Send" 
+        style={{ width:100}}/>
         </Content>
     </>
 }
