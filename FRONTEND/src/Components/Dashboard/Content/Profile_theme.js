67b998a35aec7f4e0a7bf994fee95db01807e801
Neha <93382562+Neha4581@users.noreply.github.com>
2023-09-04 18:59:01 +0530

    Profile changes by Neha

--- FRONTEND/src/Components/Dashboard/Content/Profile_theme.js
+++ FRONTEND/src/Components/Dashboard/Content/Profile_theme.js
@@ -10,12 +10,12 @@ const Profile_theme = ({ Page_title, route, ...rest }) => {
                         <div className='text-center'>
                             <i class="fa-solid fa-user display-1"></i>
                             <h3 className='text-center'>Administrator</h3>
-                        <div className='text-center'>
-                            <label><b>Email ID :</b></label>
                             <div className='text-center'>
-                                <label><b>Mobile :</b></label>
+                                <label><b>Email ID :</b></label>
+                                <div className='text-center'>
+                                    <label><b>Mobile :</b></label>
+                                </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                     <div className="row page-titles col-md-12">
