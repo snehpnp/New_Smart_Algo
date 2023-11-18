5fa8beaeeac7d9f6fa54495034d9d93e6d0ef87f
snehpnp <snehpnp@gmail.com>
2023-11-18 15:09:02 +0530

    done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -7,10 +7,11 @@ export const react_domain = `${window.location.origin}`;
 
 
 
-export const base_url = "http://localhost:7700/";
+// export const base_url = "http://localhost:7700/";
 // export const broker_url = "http://localhost:8000/";
 
 
+export const base_url = "https://trade.codingpandit.com/backend/";
 
 // export const base_url = "http://180.149.241.128:7700/";
 // export const base_url = "http://170.187.232.244:7700/";
