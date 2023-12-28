7b3ec4e8dd977b13f17abb994993bdfd324699a0
snehpnp <snehpnp@gmail.com>
2023-12-28 16:15:11 +0530

    done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -4,16 +4,16 @@
 export const react_domain = `${window.location.origin}`;
 
 
-// export const smartAlogUrl = `https://trade.pandpinfotech.com/backend/`;
-export const smartAlogUrl = `http://localhost:7700/`;
+export const smartAlogUrl = `https://trade.pandpinfotech.com/backend/`;
+// export const smartAlogUrl = `http://localhost:7700/`;
 
 
 
-export const base_url = "http://localhost:7700/";
+// export const base_url = "http://localhost:7700/";
 
 
 // export const base_url = "https://trade.pandpinfotech.com/backend/";
-// export const base_url = "https://trade.codingpandit.com/backend/";
+export const base_url = "https://trade.codingpandit.com/backend/";
 
 
 
