c61630c1cb9f8b824f407ec884149848b3d59a85
SHKGIT077 <shakirgit335@gmail.com>
2024-01-19 18:00:17 +0530

    done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -10,9 +10,9 @@ export const smartAlogUrl = `https://trade.pandpinfotech.com/backend/`;
 
 
 
-export const base_url = `${window.location.origin}/backend/`
+// export const base_url = `${window.location.origin}/backend/`
 
-//  export const base_url = "http://localhost:7700/";
+ export const base_url = "http://localhost:7700/";
 
 // export const base_url = "https://trade.pandpinfotech.com/backend/";
 // export const base_url = "https://trade.codingpandit.com/backend/";
