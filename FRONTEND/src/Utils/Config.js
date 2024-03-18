8b1013fb30c40e06c93d67cda59f2d92db751269
SHKGIT077 <shakirgit335@gmail.com>
2024-03-18 14:35:57 +0530

    done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -8,14 +8,14 @@ export const react_domain = `${window.location.origin}`;
 export const smartAlogUrl = `https://newpenal.pandpinfotech.com/backend/`;
 // export const smartAlogUrl = `http://localhost:7700/`;
 
- export const base_url = `${window.location.origin}/backend/`
+//  export const base_url = `${window.location.origin}/backend/`
 
 // export const base_url = "http://localhost:7700/";
 
 
 // export const base_url = "https://newpenal.pandpinfotech.com/backend/"
 // export const base_url = "https://trade.pandpinfotech.com/backend/";
-// export const base_url = "https://trade.codingpandit.com/backend/";
+export const base_url = "https://trade.codingpandit.com/backend/";
 // export const base_url = "https://software.oneplanetitsolution.com/backend/";
 // export const base_url = "https://software.algobullstradingsolutions.com/backend/";
 // export const base_url = "https://software.finnshri.com/backend/";
