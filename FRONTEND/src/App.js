a0519f7ccb792b16e6fca8c72a1745f9d0e386ab
snehpnp <snehpnp@gmail.com>
2024-06-22 10:47:46 +0530

    Sneh Client Done

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,95 +1,55 @@
 import Main_Router from './Routes/Route'
 import NotFound from './layout/Auth/Deactivate_Company'
-
 import React, { useState, useEffect, useRef } from 'react'
-import { useDispatch } from "react-redux";
 import * as Config from "./Utils/Config";
 import axios from "axios";
-import {
-  Get_Panel_Informtion,
-} from "../src/ReduxStore/Slice/Auth/AuthSlice";
-import $ from "jquery";
+import Loader from './Utils/Loader'; 
 
 const App = () => {
   const pageRef = useRef(null);
-  const dispatch = useDispatch()
-
   const [admin_permission, setAdmin_permission] = useState("");
+  const [loading, setLoading] = useState(true); 
 
-
-  
   const data2 = async () => {
+    const data = { "domain": Config.react_domain };
 
+    const fetchData = axios.post(`${Config.smartAlogUrl}get/panel/permission`, data, {
+      data: { data },
+    });
+
+    const delay = new Promise(resolve => setTimeout(resolve, 3000)); // 3-second delay
 
     try {
-
-      const data = { "domain": Config.react_domain }
-
-      const res = await axios.post(`${Config.smartAlogUrl}get/panel/permission`, data, {
-        data: { data },
-      })
+      const res = await Promise.all([fetchData, delay]).then(values => values[0]);
 
       if (res.data.status) {
-        setAdmin_permission(
-          res.data.data[0],
-        );
+        setAdmin_permission(res.data.data[0]);
       } else {
-        setAdmin_permission(
-          res.data,
-        );
+        setAdmin_permission(res.data);
       }
     }
     catch (err) {
-      return await err
+      console.error(err);
+    } finally {
+      setLoading(false); 
     }
-
-
   }
-
-
-
-  const getPanelDetails = async () => {
-    let domain = window.location.host
-    const req = {
-      domain: Config.react_domain
-  
-    };
-
-    await dispatch(Get_Panel_Informtion(req))
-      .unwrap()
-      .then((response) => {
-        let res = response.data[0].theme_data[0];
-        localStorage.setItem("theme", JSON.stringify(res));
-      });
-  };
-
-
-
-
-
 
   useEffect(() => {
     data2()
   }, [])
 
-
-
-
-
-
   return (
     <div id="App">
-      <div ref={pageRef} >
-        {admin_permission && admin_permission.is_active == 1 ?  <NotFound /> : <Main_Router />}
+      <div ref={pageRef}>
+        {loading ? (
+          <Loader fullPage />
+        ) : (
+          admin_permission && admin_permission.is_active == 1 ? <NotFound /> : <Main_Router />
+        )}
       </div>
-   
     </div>
   )
 }
 
 export default App
-
-
-
-
-
--- FRONTEND/src/Utils/Loader.js
+++ FRONTEND/src/Utils/Loader.js
@@ -1,18 +1,12 @@
-import React from 'react'
-// import "../App.css"
-function Loader() {
-    return (
+import React from 'react';
 
-            <div class="loader">
-            <div class="loader__image">
-                <div class="loader__coin">
-                    <img src="https://www.dropbox.com/s/fzc3fidyxqbqhnj/loader-coin.png?raw=1" alt="" />
-                </div>
-                <div class="loader__hand">
-                    <img src="https://www.dropbox.com/s/y8uqvjn811z6npu/loader-hand.png?raw=1" alt="" />
-                </div>
-        </div></div>
-    )
+
+const Loader = ({ fullPage = false }) => {
+  return (
+    <div className={fullPage ? 'full-page-loader-overlay' : 'card-loader-overlay'}>
+      <div className="loader">Loading...</div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
