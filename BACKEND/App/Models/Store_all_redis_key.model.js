6bae9694c3b62fa3595c938bff3dbe699074e9b7
SHKGIT007 <shkgit007@gmail.com>
2024-07-05 18:53:46 +0530

    done

--- BACKEND/App/Models/Store_all_redis_key.model.js
+++ BACKEND/App/Models/Store_all_redis_key.model.js
@@ -4,6 +4,7 @@ const StoreAllRedisKeySchema = new mongoose.Schema({
     token: {
         type: String,
         required: true,
+        unique: true,
         index: true
     },
     key_symbol: {
