48f10cc7920f4090a2aa129c80e784b1c5961d4f
SHKGIT007 <shkgit007@gmail.com>
2024-08-20 16:28:50 +0530

    done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -7,7 +7,7 @@ export const smartAlogUrl = `https://newpenal.pandpinfotech.com/backend/`;
 // export const smartAlogUrl = "http://localhost:7700/";
 
 
-// export const base_url = `${window.location.origin}/backend/` 
+export const base_url = `${window.location.origin}/backend/` 
 // export const base_url = "https://trade.pandpinfotech.com/backend/";
 // export const base_url = "http://localhost:7700/";
 // export const base_url = "http://185.209.75.22:7700/";
@@ -17,7 +17,7 @@ export const smartAlogUrl = `https://newpenal.pandpinfotech.com/backend/`;
 // export const base_url = "https://software.oneplanetitsolution.com/backend/";
 // export const base_url = "https://software.algobullstradingsolutions.com/backend/";
 // export const base_url = "https://software.finnshri.com/backend/";
-export const base_url = "https://software.corebizinfotech.com/backend/";
+// export const base_url = "https://software.corebizinfotech.com/backend/";
 // export const base_url = "https://software.growskyinfotech.com/backend/";
 // export const base_url = "https://software.sumedhainn.com/backend/";
 // export const base_url = "https://software.growupalgo.com/backend/";
