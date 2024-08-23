5380e25ff0005d6e0ad70515d1f751bf99db81b4
snehpnp <snehpnp@gmail.com>
2024-08-23 14:42:19 +0530

    Done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -1,5 +1,5 @@
-export const react_domain = `${window.location.origin}`;
-// export const react_domain = `https://software.one-algo.com`;
+// export const react_domain = `${window.location.origin}`;
+export const react_domain = `https://software.growskyinfotech.com`;
 
 
 
@@ -9,7 +9,7 @@ export const smartAlogUrl = `https://newpenal.pandpinfotech.com/backend/`;
 
 // export const base_url = `${window.location.origin}/backend/` 
 // export const base_url = "https://trade.pandpinfotech.com/backend/";
-export const base_url = "http://localhost:7700/";
+// export const base_url = "http://localhost:7700/";
 // export const base_url = "http://185.209.75.22:7700/";
 // export const base_url = "https://newpenal.pandpinfotech.com/backend/"
 // export const base_url = "http://185.209.75.22:7700/";
@@ -18,7 +18,7 @@ export const base_url = "http://localhost:7700/";
 // export const base_url = "https://software.algobullstradingsolutions.com/backend/";
 // export const base_url = "https://software.finnshri.com/backend/";
 // export const base_url = "https://software.corebizinfotech.com/backend/";
-// export const base_url = "https://software.growskyinfotech.com/backend/";
+export const base_url = "https://software.growskyinfotech.com/backend/";
 // export const base_url = "https://software.sumedhainn.com/backend/";
 // export const base_url = "https://software.growupalgo.com/backend/";
 // export const base_url = "https://software.fincodify.com/backend/";
