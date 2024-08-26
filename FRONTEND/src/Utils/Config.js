3af48de52dc823106ba17727cae4ce52eed579d6
SHKGIT007 <shkgit007@gmail.com>
2024-08-26 17:34:30 +0530

    Done

--- FRONTEND/src/Utils/Config.js
+++ FRONTEND/src/Utils/Config.js
@@ -1,5 +1,5 @@
-// export const react_domain = `${window.location.origin}`;
-export const react_domain = `https://software.one-algo.com`;
+export const react_domain = `${window.location.origin}`;
+// export const react_domain = `https://software.one-algo.com`;
 
 
 
